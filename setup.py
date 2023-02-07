c2762bf8888b2845f38c43237ea7155d1eb66730
LewisDrysdale <lewisdrysdale@hotmail.co.uk>
2023-02-07 10:13:20 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A collection of toolls to process and analyse data from Teledyne Gvaia Offshore Surveyor',
+    author='Lewis Drysdale',
+    license='MIT',
+)
