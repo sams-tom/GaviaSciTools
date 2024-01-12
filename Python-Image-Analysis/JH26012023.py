92ce79aed4c49a475131592502ffab22ae41a706
tom-braz <tom.brazinski@gmai.com>
2024-01-12 11:57:02 +0000

    updated with more text explaining

--- Python-Image-Analysis/JH26012023.py
+++ Python-Image-Analysis/JH26012023.py
@@ -18,43 +18,42 @@ from scipy import ndimage
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+#must have exiftool downloaded and saved in working dictionary and PyExifTool python module downloaded
 import exiftool 
 import numpy, PIL
 from PIL import Image
 import re
 
-#Path to process with two \\ at end, eg path=r"T:\Nicol\20190720141751\\"
+#Path to process with /, using \ alone will fail
 path=r"D:/Strangford Lough_June2021_TOM/AUV Data/images/20210615075602/"
-#Output folder eg "\12052022\\"
-#save_folder="\20230120123513_Proc\\"
+#As above, use /
 save_folder='answers/'
 #Image Enhancement method, 'CLAHE' or 'AverageSubtraction'
 ImageEnhancement='AverageSubtraction'
-#outpath
+#outpath, this is where it will save stuff
 outpath=os.path.join(path,save_folder)
 print(outpath)
 print(path)
 #path to ExifTool:
 exiftool_path = r'D:/Strangford Lough_June2021_TOM/AUV Data/images/20210615075602/'
-#Makes save folder
+#Makes save folder, if there is already a folder it will just ignore this and print an error
 try: 
-    #os.mkdir(path+save_folder) 
+   
     os.mkdir(os.path.join(path,save_folder))
 except OSError as error: 
     print(error)
     
     
- #liests files   
+ #lists files of JPGs  
 files=glob.glob(path+'*.jpg')
 
 
 
 # Assuming all images are the same size, get dimensions of first image
-#w,h=Image.open(files[0]).size
 h,w,d=(io.imread(files[0])).shape
 
 N=len(files)
-# Create a numpy array of floats to store the average (assume RGB images)
+# Create a numpy array of floats to store the average (assume RGB images), numpy.float has been dropped
 arr=numpy.zeros((h,w,3),float)
 
 # Build up average pixel intensities, casting each image as an array of floats
@@ -65,42 +64,40 @@ for im in files:
 # Round values in array and cast as 8-bit integer
 arr1=numpy.array(numpy.round(arr),dtype=numpy.uint8)
 
-# Generate, save and preview final image
+# Generate, save and preview final image (this is the average which is removed from all photos)
 out=Image.fromarray(arr1,mode="RGB")
 out.save(os.path.join(outpath,"Average.png"))
 out.show()
 
-
+#creates a dataframe to save meta data
 df = pd.DataFrame(columns=['Image_Name','path', 'altitude', 'depth','heading','lat','lon','pitch','roll','surge','sway'])
 
 
 
 loopy=range(len(files))
 
+###I added this to work out how to get the exiftool to work
 os.chdir(exiftool_path)
 # Get the current working directory
 current_directory = os.getcwd()
 
 # Print the current working directory
+#EXIFTOOL.EXE  MUST BE DOWNLOADED AND SAVED IN THIS FILE
+#Download from: https://exiftool.org/
 print("Current Working Directory:", current_directory)
 
-
+#extracts metadata
 with exiftool.ExifToolHelper() as et:
         metadata = et.get_metadata(files)
 
+ #loops over all photos
 for i in loopy:
+    #this block moves to file i then prints its processing this
     file=files[i]
     files1 = [file]
     print('processing image  '+file)
 
-    #with exiftool.ExifToolHelper() as et:
-    #    metadata = et.get_metadata(files1)
-        
-        
-    #for d in metadata:
-    #    print("{:20.20} {:20.20}".format(d["SourceFile"],
-    #                                     d["EXIF:DateTimeOriginal"]))
-    
+   #searches the comment to look for something in between '<altitude>(.*)</altitude>' etc. ad saves it in df
     comment=metadata[i]['File:Comment']
     altitude = re.search('<altitude>(.*)</altitude>', comment).group(1)
     depth = re.search('<depth>(.*)</depth>', comment).group(1)
@@ -112,7 +109,7 @@ for i in loopy:
     surge = re.search('<surge>(.*)</surge>', comment).group(1)
     sway = re.search('<sway>(.*)</sway>', comment).group(1)
     
-    
+    #These convert into latitude and longitude
     signlat = 1
     if lat[-1] == "S":
         signlat = -1    
@@ -126,9 +123,8 @@ for i in loopy:
     lonCor = signlon * (float(lon[:3]) + float(lon[3:lenlon-2])/60.0)
     
     
-    #im1=numpy.array(Image.open(file),dtype=numpy.float)
   
-    
+    #these undertake the corrections defined above on the images
     if ImageEnhancement=="AverageSubtraction":
         im1=numpy.array(io.imread(file),dtype=float)
         imcor=im1-arr
@@ -141,10 +137,10 @@ for i in loopy:
     
 
     #
-    
+    #it finally saves these photos
     io.imsave(os.path.join(path,save_folder, os.path.basename(file)),out2)
 
-    #df.loc[i] = [os.path.basename(file),file,altitude,depth,heading,latCor,lonCor,str(float(pitch)+270),roll,surge,sway]
+    #saves the lat etc into the dataframe
     df.loc[i] = [os.path.basename(file),file,altitude,str(-float(depth)),heading,latCor,lonCor,pitch,roll,surge,sway]
     
     
